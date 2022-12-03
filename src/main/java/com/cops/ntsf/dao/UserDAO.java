fb09d4f703ae4aa669ad3ad2a4d631681838ff55
AvishiGan <avishiganepola@gmail.com>
2022-12-03 09:39:11 +0530

    Add name, address for sql string

--- src/main/java/com/cops/ntsf/dao/UserDAO.java
+++ src/main/java/com/cops/ntsf/dao/UserDAO.java
@@ -21,10 +21,12 @@ public class UserDAO {
         Connection dbConn = Database.getConnection();
 
 //        String sql = "INSERT INTO user (nic, email, mobile_no, user_type) VALUES (?, ?, ?, ?)";
-        String sql = "INSERT INTO user (nic, email, mobile_no, user_type) VALUES (?, ?, ?, ?)";
+        String sql = "INSERT INTO user (name, address, nic, email, mobile_no, user_type) VALUES (?, ?, ?, ?, ?, ?)";
 
         try {
             PreparedStatement preparedStatement = dbConn.prepareStatement(sql);
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(1, user.getAddress());
             preparedStatement.setString(1, user.getNic());
             preparedStatement.setString(2, user.getEmail());
             preparedStatement.setString(3, user.getMobileNo());
@@ -51,6 +53,8 @@ public class UserDAO {
 
             while (resultSet.next()) {
                 user.setUserId(resultSet.getString("user_id"));
+                user.setName(resultSet.getString("name"));
+                user.setAddress(resultSet.getString("address"));
                 user.setNic(resultSet.getString("nic"));
                 user.setEmail(resultSet.getString("email"));
                 user.setMobileNo(resultSet.getString("mobile_no"));
