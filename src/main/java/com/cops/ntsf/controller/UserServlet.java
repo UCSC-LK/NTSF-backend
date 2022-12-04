0e57b7707c428705de617c5a70d13875364a933e
AvishiGan <avishiganepola@gmail.com>
2022-12-04 22:07:26 +0530

    Get request parameters login id and user id

--- src/main/java/com/cops/ntsf/controller/UserServlet.java
+++ src/main/java/com/cops/ntsf/controller/UserServlet.java
@@ -1,4 +1,15 @@
 package com.cops.ntsf.controller;
 
-public class UserServlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class UserServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String loginId = req.getParameter("login_id");
+        String userId = req.getParameter("user_id");
+    }
 }
