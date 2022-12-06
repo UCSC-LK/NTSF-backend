140d4b7c437e3f9192fef27bfe8ab533050af5c1
AvishiGan <avishiganepola@gmail.com>
2022-12-06 06:41:13 +0530

    Create fetchDriverLicence function

--- src/main/java/com/cops/ntsf/dao/DriverDAO.java
+++ src/main/java/com/cops/ntsf/dao/DriverDAO.java
@@ -22,9 +22,9 @@ public class DriverDAO {
             preparedStatement.setString(1, driver.getLicenceNo());
             ResultSet resultSet = preparedStatement.executeQuery();
 
-            while (resultSet.next()){
-               // driver.setLicenceExpiryDate(LocalDate.parse(resultSet.getString("licence_expire_date")));
-               // driver.setLicenceIssueDate(LocalDate.parse(resultSet.getString("licence_issue_date")));
+            while (resultSet.next()) {
+                // driver.setLicenceExpiryDate(LocalDate.parse(resultSet.getString("licence_expire_date")));
+                // driver.setLicenceIssueDate(LocalDate.parse(resultSet.getString("licence_issue_date")));
                 driver.setUserId(resultSet.getString("user_id"));
             }
         } catch (SQLException e) {
@@ -37,7 +37,7 @@ public class DriverDAO {
 
         String sql = "INSERT INTO driver(user_id, licence_no) VALUES (?, ?)";
 
-        try{
+        try {
             PreparedStatement preparedStatement = dbConn.prepareStatement(sql);
             preparedStatement.setString(1, driver.getUserId());
             preparedStatement.setString(2, driver.getLicenceNo());
@@ -46,5 +46,26 @@ public class DriverDAO {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void fetchDriverLicence(Driver driver) {
+        Connection dbConn = Database.getConnection();
+
+        String sql = "SELECT * FROM driver WHERE user_id = ?";
+
+        try {
+            PreparedStatement preparedStatement = dbConn.prepareStatement(sql);
+            preparedStatement.setString(1, driver.getUserId());
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                driver.setLicenceNo(resultSet.getString("licence_no"));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
