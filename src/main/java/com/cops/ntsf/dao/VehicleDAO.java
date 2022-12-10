0591b517bc43cad990dbabc9fce702b038fdc23e
AvishiGan <avishiganepola@gmail.com>
2022-12-10 18:07:10 +0530

    Create vehicle dao

---
+++ src/main/java/com/cops/ntsf/dao/VehicleDAO.java
@@ -0,0 +1,32 @@
+package com.cops.ntsf.dao;
+
+import com.cops.ntsf.model.Vehicle;
+import com.cops.ntsf.util.Database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class VehicleDAO {
+
+    public void getVehicleFromVehicleNo(Vehicle vehicle) {
+        Connection dbConn = Database.getConnection();
+
+        String sql = "SELECT * FROM vehicle WHERE vehicle_no = ?";
+
+        try {
+            PreparedStatement preparedStatement = dbConn.prepareStatement(sql);
+            preparedStatement.setString(1, vehicle.getVehicleNo());
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                // driver.setLicenceExpiryDate(LocalDate.parse(resultSet.getString("licence_expire_date")));
+                // driver.setLicenceIssueDate(LocalDate.parse(resultSet.getString("licence_issue_date")));
+                vehicle.setUserId(resultSet.getString("user_id"));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
