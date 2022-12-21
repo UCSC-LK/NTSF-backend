03b413f910ff926df0aedd8835c35dbfe1e7a60e
AvishiGan <avishiganepola@gmail.com>
2022-12-21 06:00:13 +0530

    Create fine servlet

---
+++ src/main/java/com/cops/ntsf/controller/FineServlet.java
@@ -0,0 +1,37 @@
+package com.cops.ntsf.controller;
+
+import com.cops.ntsf.model.Fine;
+import com.google.gson.Gson;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+public class FineServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // Get request parameters
+        String userId = req.getParameter("user_id");
+
+//        UserService userService = new UserService();
+        Fine fine = new Fine(userId);
+        try {
+            fine.getUserFinesInfo();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Output response
+        PrintWriter out = resp.getWriter();
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("utf-8");
+
+        out.write(new Gson().toJson(fine));
+        out.close();
+    }
+}
