40055004e67e7cb4d1745613e26698b5ad633d49
Suraif Muhammad <muhammadsuraif2000@gmail.com>
2023-01-02 12:22:32 +0530

    make minute change in PoliceDAO

--- src/main/java/com/cops/ntsf/dao/PolicemanDAO.java
+++ src/main/java/com/cops/ntsf/dao/PolicemanDAO.java
@@ -13,26 +13,31 @@ import java.util.List;
 public class PolicemanDAO {
     private static Connection dbConnect = null;
 
-    public static boolean addPoliceman(String name, String police_id, String nic, String rank, String police_station)
-    {
+    public static boolean addPoliceman(String name, String police_id, String nic, String rank, String police_station) {
         boolean isSuccess = false;
+
+        return isSuccess;
     }
-    public static List<Policeman> viewPoliceman(){
+
+
+
+
+    
+    public static List<Policeman> viewPoliceman () {
         ArrayList<Policeman> policemanInformation = new ArrayList<>();
 
-        try{
+        try {
             dbConnect = DBConnect.getConnection();
             Statement statement = dbConnect.createStatement();
             String sql = "select * from policeman";
             ResultSet rs = statement.executeQuery(sql);
 
-            while (rs.next())
-            {
-               String name = rs.getString(1);
-               String police_id = rs.getString(2);
-               String nic = rs.getString(3);
-               String rank = rs.getString(4);
-               String police_station = rs.getString(5);
+            while (rs.next()) {
+                String name = rs.getString(1);
+                String police_id = rs.getString(2);
+                String nic = rs.getString(3);
+                String rank = rs.getString(4);
+                String police_station = rs.getString(5);
 
                 Policeman policemanInfo = new Policeman(name, police_id, nic, rank, police_station);
                 policemanInformation.add(policemanInfo);
@@ -43,5 +48,6 @@ public class PolicemanDAO {
             e.printStackTrace();
         }
         return policemanInformation;
+
     }
 }
