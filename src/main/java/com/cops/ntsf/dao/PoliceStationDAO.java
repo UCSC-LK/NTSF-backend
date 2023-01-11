2a42f4fe999220687a653681befdb1ba8c1be4e9
Suchith Sandunika <91415039+SSE77@users.noreply.github.com>
2023-01-11 06:25:29 +0530

    Merge pull request #12 from Suraif16/Suchithbackend
    added the station java files

---
+++ src/main/java/com/cops/ntsf/dao/PoliceStationDAO.java
@@ -0,0 +1,46 @@
+package com.cops.ntsf.dao;
+
+import com.cops.ntsf.util.DBConnect;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+/*import com.cops.ntsf.model.Complaint;
+import com.cops.ntsf.util.DBConnect;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;*/
+
+public class PoliceStationDAO
+{
+    private static Connection dbConnect = null;
+
+    public static boolean AddPoliceStation(/*String station_id,*/ String name) {
+        boolean isSuccess = false;
+
+        try {
+            dbConnect = DBConnect.getConnection();
+            Statement stmt = dbConnect.createStatement();
+            String sql = "insert into complaint values ('" + name + "')";
+            int rs = stmt.executeUpdate(sql); //returns 1 if insertion is successful(since only 1 row is affected)
+
+            if (rs > 0) {
+                isSuccess = true;
+            } else {
+                isSuccess = false;
+            }
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return isSuccess;
+        }
+
+    /*public static boolean ViewPoliceStation(){
+
+    }*/
+}
