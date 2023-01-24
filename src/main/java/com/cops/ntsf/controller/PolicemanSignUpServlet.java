d95503d96d1df396d6196aad8c0c4d281ae9793e
AvishiGan <avishiganepola@gmail.com>
2023-01-24 18:45:02 +0530

    Create PolicemanSignUpServlet

---
+++ src/main/java/com/cops/ntsf/controller/PolicemanSignUpServlet.java
@@ -0,0 +1,33 @@
+package com.cops.ntsf.controller;
+
+import com.cops.ntsf.model.Policeman;
+import com.cops.ntsf.service.PoliceService;
+import com.google.gson.Gson;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class PolicemanSignUpServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        // Get request parameters
+        String policeId = req.getParameter("police_id");
+        String nic = req.getParameter("nic");
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+
+        PoliceService policeService = new PoliceService();
+        Policeman policeman = policeService.getPolicemanSignedUp(policeId, nic, email, password);
+
+        // Output response
+        PrintWriter out = resp.getWriter();
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("utf-8");
+
+        out.write(new Gson().toJson(policeman));
+        out.close();
+    }
+}
