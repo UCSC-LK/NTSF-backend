cd68f2ef0ba5c01fc32b40da88b126a9e4bb3e56
Shanaka Fernando <shanaka2686@gmail.com>
2023-01-26 12:29:23 +0530

    Add the Edit Servlet to OffenceServlet.java File

--- src/main/java/com/cops/ntsf/controller/OffenceServlet.java
+++ src/main/java/com/cops/ntsf/controller/OffenceServlet.java
@@ -1,6 +1,7 @@
 package com.cops.ntsf.controller;
 
 
+import com.cops.ntsf.dao.OffenceDAO;
 import com.cops.ntsf.model.Offence;
 import com.cops.ntsf.service.OffenceService;
 import com.google.gson.Gson;
@@ -13,12 +14,12 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 
-public class AddOffenceServlet extends HttpServlet {
+public class OffenceServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         // Get request parameters
-//        UserType userType = UserType.fromId(Integer.parseInt(req.getParameter("fine_type")));
+//       UserType userType = UserType.fromId(Integer.parseInt(req.getParameter("fine_type")));
         Integer offenceNo = Integer.valueOf(req.getParameter("offence_no"));
         String offenceType = req.getParameter("offence_type");
         Integer pointWeight = Integer.valueOf(req.getParameter("point_weight"));
@@ -36,4 +37,36 @@ public class AddOffenceServlet extends HttpServlet {
         out.write(new Gson().toJson(offence));
         out.close();
     }
+
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+
+        resp.setContentType("text/html");
+        PrintWriter out=resp.getWriter();
+
+        Integer offenceNo = Integer.valueOf(req.getParameter("offence_no"));
+        String offenceType = req.getParameter("offence_type");
+        Integer pointWeight = Integer.valueOf(req.getParameter("point_weight"));
+        String description = req.getParameter("description");
+        Integer amount = Integer.valueOf(req.getParameter("amount"));
+
+
+        Offence offence=new Offence();
+        offence.setOffenceNo(offenceNo);
+        offence.setOffenceType(offenceType);
+        offence.setPointWeight(pointWeight);
+        offence.setDescription(description);
+        offence.setAmount(amount);
+
+        int status= OffenceDAO.updateOffenceInfo(offence);
+        if(status>0){
+            resp.sendRedirect("ViewServlet");
+        }else{
+            out.println("Sorry! unable to update record");
+        }
+        out.close();
+    }
+
+
+
 }
+
