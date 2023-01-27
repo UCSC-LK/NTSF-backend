14e2fad02c4740856926475502b9eddc0a96b881
AvishiGan <avishiganepola@gmail.com>
2023-01-27 14:11:31 +0530

    Change insert sql dialect

--- src/main/java/com/cops/ntsf/dao/PolicemanDAO.java
+++ src/main/java/com/cops/ntsf/dao/PolicemanDAO.java
@@ -13,11 +13,11 @@ public class PolicemanDAO {
         Connection dbConnect = null;
         try {
             dbConnect = DBConnect.getConnection();
-            String sql = "INSERT into policeman (name, police_id, nic, rank, police_station) VALUES (?, ?, ?, ?, ?)";
+            String sql = "INSERT into policeman (police_id, name, nic, rank, police_station) VALUES (?, ?, ?, ?, ?)";
             PreparedStatement preparedStatement = dbConnect.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
-            preparedStatement.setString(1, policeman.getName());
-            preparedStatement.setString(2, policeman.getPoliceId());
+            preparedStatement.setString(1, policeman.getPoliceId());
+            preparedStatement.setString(2, policeman.getName());
             preparedStatement.setString(3, policeman.getNic());
             preparedStatement.setString(4, policeman.getRank());
             preparedStatement.setString(5, policeman.getPoliceStation());
@@ -40,45 +40,24 @@ public class PolicemanDAO {
         return null;
     }
 
-    public void insertTrafficPoliceInfo(TrafficPolice trafficPolice) {
-        Connection dbConn = Database.getConnection();
-
-//        String sql = "INSERT INTO user (nic, email, mobile_no, user_type) VALUES (?, ?, ?, ?)";
-        String sql = "INSERT INTO taffic_police (name, police_id, nic, rank, police_station) VALUES (?, ?, ?, ?, ?)";
-
-        try {
-            PreparedStatement preparedStatement = dbConn.prepareStatement(sql);
-            preparedStatement.setString(1, trafficPolice.getName());
-            preparedStatement.setString(2, trafficPolice.getPoliceId());
-            preparedStatement.setString(3,trafficPolice.getNic());
-            preparedStatement.setString(3,trafficPolice.getRank());
-            preparedStatement.setString(3,trafficPolice.getPoliceStation());
-
-            preparedStatement.executeUpdate();
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void insertPolicemanInfo(Policeman policeman) {
-
-        Connection dbConn = Database.getConnection();
-
-        String sql = "INSERT INTO policeman (police_id, nic, email) VALUES (?, ?, ?, ?, ?)";
-
-        try {
-            PreparedStatement preparedStatement = dbConn.prepareStatement(sql);
-            preparedStatement.setString(1,policeman.getPoliceId());
-            preparedStatement.setString(2,policeman.getNic());
-            preparedStatement.setString(3,policeman.getEmail());
-
-            preparedStatement.executeUpdate();
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
+//    public void insertPolicemanInfo(Policeman policeman) {
+//
+//        Connection dbConn = Database.getConnection();
+//
+//        String sql = "INSERT INTO policeman (police_id, nic, email) VALUES (?, ?, ?, ?, ?)";
+//
+//        try {
+//            PreparedStatement preparedStatement = dbConn.prepareStatement(sql);
+//            preparedStatement.setString(1,policeman.getPoliceId());
+//            preparedStatement.setString(2,policeman.getNic());
+//            preparedStatement.setString(3,policeman.getEmail());
+//
+//            preparedStatement.executeUpdate();
+//
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
 
     public void updatePolicemanRankStation(Policeman policeman) {
         Connection dbConn = Database.getConnection();
