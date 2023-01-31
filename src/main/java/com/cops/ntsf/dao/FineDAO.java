a725979a0e7db98204873d4177e408a96291f2cd
Shanaka Fernando <shanaka2686@gmail.com>
2023-01-31 18:07:01 +0530

    Add the Add_Fine Function to the FineDAO.java

--- src/main/java/com/cops/ntsf/dao/FineDAO.java
+++ src/main/java/com/cops/ntsf/dao/FineDAO.java
@@ -1,6 +1,7 @@
 package com.cops.ntsf.dao;
 
 import com.cops.ntsf.model.Fine;
+import com.cops.ntsf.model.Offence;
 import com.cops.ntsf.util.Database;
 
 import java.sql.Connection;
@@ -10,6 +11,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class  FineDAO {
+
+
     public ArrayList<Fine> fetchUserFinesInfo(Fine fine) throws SQLException {
         Connection dbConn = Database.getConnection();
 
@@ -26,15 +29,45 @@ public class  FineDAO {
         while (resultSet.next()) {
             Fine nextFine;
             nextFine = new Fine(fine.getUserId());
-            nextFine.setTicketNo(Integer.valueOf(resultSet.getString("ticket_no")));
+            //nextFine.setTicketNo(Integer.valueOf(resultSet.getString("ticket_no")));
             nextFine.setFineNo(Integer.valueOf(resultSet.getString("fine_no")));
-            nextFine.setDate(resultSet.getDate("date"));
+            nextFine.setFineDate(resultSet.getDate("fine_date"));
             nextFine.setDueDate(resultSet.getDate("due_date"));
-            nextFine.setFineAmount(resultSet.getBigDecimal("fine_amount"));
+            nextFine.setFineAmount(resultSet.getInt("fine_amount"));
             nextFine.setPaymentStatus(resultSet.getString("payment_status"));
 
             finesList.add(nextFine);
         }
         return finesList;
     }
+
+
+    public void setOffenceInfo(Fine fine) {
+        Connection dbConn = Database.getConnection();
+
+        String sql = "INSERT INTO fine(fine_no, fine_type,user_id, fine_date ,due_date,fine_amount,payment_status,police_id,fine_time ) VALUES (?, ?,?,?,?,?,?,?,?)";
+
+        try {
+            PreparedStatement preparedStatement = dbConn.prepareStatement(sql);
+
+            preparedStatement.setInt(1, fine.getFineNo());
+            preparedStatement.setString(2, fine.getFineType());
+            preparedStatement.setString(3, fine.getUserId());
+            preparedStatement.setDate(4, fine.getFineDate());
+            preparedStatement.setDate(5, fine.getDueDate());
+            preparedStatement.setInt(6, fine.getFineAmount());
+            preparedStatement.setString(7, fine.getPaymentStatus());
+            preparedStatement.setString(8, fine.getPoliceID());
+            preparedStatement.setTime(9, fine.getFineTime());
+
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+
 }
