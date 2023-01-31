41be6add70e168ff52f36cca63afa0466841ea4b
Shanaka Fernando <shanaka2686@gmail.com>
2023-01-31 18:09:50 +0530

    Add the doPost Method to the FineServlet.java

--- src/main/java/com/cops/ntsf/controller/FineServlet.java
+++ src/main/java/com/cops/ntsf/controller/FineServlet.java
@@ -1,6 +1,8 @@
 package com.cops.ntsf.controller;
 
 import com.cops.ntsf.model.Fine;
+import com.cops.ntsf.model.Offence;
+import com.cops.ntsf.service.OffenceService;
 import com.google.gson.Gson;
 
 import javax.servlet.ServletException;
@@ -13,6 +15,35 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class FineServlet extends HttpServlet {
+
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // Get request parameters
+//       UserType userType = UserType.fromId(Integer.parseInt(req.getParameter("fine_type")));
+        Integer offenceNo = Integer.valueOf(req.getParameter("offence_no"));
+        String offenceType = req.getParameter("offence_type");
+        Integer pointWeight = Integer.valueOf(req.getParameter("point_weight"));
+        String description = req.getParameter("description");
+        Integer amount = Integer.valueOf(req.getParameter("amount"));
+
+        OffenceService offenceService = new OffenceService();
+        Offence offence = offenceService.insertOffenceInfo(offenceNo, offenceType, pointWeight, description, amount);
+
+        // Output response
+        PrintWriter out = resp.getWriter();
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("utf-8");
+
+        out.write(new Gson().toJson(offence));
+        out.close();
+    }
+
+
+
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
