d14ae1b6ef9c929528717bbf18d4c2530b82d69d
AvishiGan <avishiganepola@gmail.com>
2023-02-06 13:57:42 +0530

    Comment FineService temporarily

--- src/main/java/com/cops/ntsf/service/FineService.java
+++ src/main/java/com/cops/ntsf/service/FineService.java
@@ -1,16 +1,12 @@
 package com.cops.ntsf.service;
 
-import com.cops.ntsf.model.Fine;
-
-import java.text.ParseException;
-
-public class FineService {
-
-    public Fine insertFineInfo(Integer fineNo, String fineType, String userId, Integer fineAmount, String paymentStatus, String policeId) throws ParseException {
-
-        Fine fine = new Fine(fineNo, fineType, userId, fineAmount, paymentStatus, policeId, fineType);
-        fine.setFineInfo();
-
-        return fine;
-    }
-}
+//public class FineService {
+//
+//    public Fine insertFineInfo(Integer fineNo, String fineType, String userId, Integer fineAmount, String paymentStatus, String policeId) throws ParseException {
+//
+//        Fine fine = new Fine(fineNo, fineType, userId, fineAmount, paymentStatus, policeId, fineType);
+//        fine.setFineInfo();
+//
+//        return fine;
+//    }
+//}
