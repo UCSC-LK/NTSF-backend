07305fe64058ec89e0f22834897d8a060f4a5a6b
Suraif Muhammad <muhammadsuraif2000@gmail.com>
2023-02-11 19:44:38 +0530

    added policestationoption list servlet

--- src/main/java/com/cops/ntsf/controller/PolicemanServlet.java
+++ src/main/java/com/cops/ntsf/controller/PolicemanServlet.java
@@ -142,6 +142,29 @@ public class PolicemanServlet extends HttpServlet {
         out.close();
 
     }
+
+    protected void loadPoliceStationOptionsList(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try{
+            PrintWriter out = response.getWriter();
+            response.setContentType("text/html");
+
+            HttpSession session = request.getSession(false);
+
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("serverResponse", "Allowed");
+
+            Policeman policeman = new Policeman();
+            JSONArray policeStationOptionsList = policeman.getPoliceStationOptionsList();
+
+            jsonObject.put("List", policeStationOptionsList );
+
+            out.write(jsonObject.toString());
+            out.close();
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
     protected void fetchPoliceman(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
         try{
             PrintWriter out = response.getWriter();
@@ -348,6 +371,10 @@ public class PolicemanServlet extends HttpServlet {
         else if(action.equals("addPoliceman")) {
             addPoliceman(request, response);
         }
+        else if(action.equals("loadPoliceStationOptionsList"))
+        {
+            loadPoliceStationOptionsList(request, response);
+        }
         else if (action.equals("viewPoliceman"))
         {
             viewPoliceman(request, response);
@@ -385,7 +412,6 @@ public class PolicemanServlet extends HttpServlet {
             System.out.println("Hi from Email Checking servelet");
         }
     }
-
     private boolean checkValidations(String name, String police_id, String nic, String mobile_number, String email, String rank, String police_station) {
         boolean flagName = false; //flag = true means name validation is passed
         boolean flagPolice_ID = false; //flag = true means Police ID validation is passed
