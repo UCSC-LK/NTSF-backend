20e6f6c54d2af95b9e3bc9ea1c209754c553a135
Suraif Muhammad <muhammadsuraif2000@gmail.com>
2023-02-13 13:09:55 +0530

    otp email

---
+++ src/main/java/com/cops/ntsf/service/Email.java
@@ -0,0 +1,58 @@
+package com.cops.ntsf.service;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class Email {
+    private final String username = "educlickorg@gmail.com";
+    private final String password = "oumpnrpmenvekbil";
+    private Session session = null;
+
+    public Email(){
+
+
+
+        Properties properties = new Properties();
+        properties.put("mail.smtp.host" , "smtp.gmail.com");
+        properties.put("mail.smtp.port" , "587");
+        properties.put("mail.smtp.auth" , "true");
+        properties.put("mail.smtp.starttls.enable" , "true"); //TLS
+
+        session = Session.getInstance( properties ,
+                new javax.mail.Authenticator(){
+                    protected PasswordAuthentication getPasswordAuthentication(){
+                        return new PasswordAuthentication( username , password );
+                    }
+                }
+        );
+
+    }
+
+    public void sendMail( String toEmail , String subject , String textMessage ){
+
+
+        try{
+            Message message = new MimeMessage( session );
+            message.setFrom( new InternetAddress("EduClick") );
+            message.setRecipients(
+                    Message.RecipientType.TO,
+                    InternetAddress.parse(toEmail)
+            );
+
+            message.setSubject(subject);
+            message.setText( textMessage );
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            System.out.println("error");
+        }
+
+        System.out.println("complete");
+    }
+}
