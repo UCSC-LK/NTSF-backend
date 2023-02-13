f6e8133ce0bd67d97b222eadac895200b765db84
Suraif Muhammad <muhammadsuraif2000@gmail.com>
2023-02-13 23:32:16 +0530

    added email

--- src/main/java/com/cops/ntsf/service/Email.java
+++ src/main/java/com/cops/ntsf/service/Email.java
@@ -12,8 +12,6 @@ public class Email {
 
     public Email(){
 
-
-
         Properties properties = new Properties();
         properties.put("mail.smtp.host" , "smtp.gmail.com");
         properties.put("mail.smtp.port" , "587");
