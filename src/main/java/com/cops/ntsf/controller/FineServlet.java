1e03c8d3723a2a0618ec17ce40a18e8e980edbf2
AvishiGan <avishiganepola@gmail.com>
2023-02-27 12:51:23 +0530

    Create add fines function in the servlet

--- src/main/java/com/cops/ntsf/controller/FineServlet.java
+++ src/main/java/com/cops/ntsf/controller/FineServlet.java
@@ -1,7 +1,9 @@
 package com.cops.ntsf.controller;
 
 import com.cops.ntsf.constants.FineType;
+import com.cops.ntsf.constants.PaymentStatus;
 import com.cops.ntsf.model.Fine;
+import com.cops.ntsf.service.FineService;
 import com.google.gson.Gson;
 
 import javax.servlet.ServletException;
@@ -10,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Date;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -44,5 +47,30 @@ public class FineServlet extends HttpServlet {
 
         out.write(new Gson().toJson(finesList));
         out.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doPost(req, resp);
+        // Get request parameters
+        String ticketNo = req.getParameter("ticket_no");
+        String fineNo = req.getParameter("fine_no");
+        Date date = Date.valueOf(req.getParameter("date"));
+        Date dueDate = Date.valueOf(req.getParameter("due_date"));
+        String fineAmount = req.getParameter("fine_amount");
+        PaymentStatus paymentStatus = PaymentStatus.valueOf(req.getParameter("payment_status"));
+        FineType fineType = FineType.valueOf(req.getParameter("fine_type"));
+
+        FineService fineService = new FineService();
+        Fine fine = fineService.insertFineInfo(ticketNo, fineNo, date, dueDate, fineAmount, paymentStatus, fineType);
+
+        // Output response
+        PrintWriter out = resp.getWriter();
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("utf-8");
+
+        out.write(new Gson().toJson(fine));
+        out.close();
+
     }
 }
