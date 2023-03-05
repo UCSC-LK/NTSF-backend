ee017cee0b43e92a447907739cf8c22ae2b863d1
AvishiGan <avishiganepola@gmail.com>
2023-03-06 03:30:04 +0530

    Made few changes in other functions and created a new function fetchVehicleInfo

--- src/main/java/com/cops/ntsf/dao/VehicleDAO.java
+++ src/main/java/com/cops/ntsf/dao/VehicleDAO.java
@@ -13,7 +13,7 @@ public class VehicleDAO {
     public void getVehicleFromVehicleNo(Vehicle vehicle) {
         Connection dbConn = Database.getConnection();
 
-        String sql = "SELECT * FROM vehicle WHERE vehicle_no = ?";
+        String sql = "SELECT * FROM vehicle WHERE register_no = ?";
 
         try {
             PreparedStatement preparedStatement = dbConn.prepareStatement(sql);
@@ -21,8 +21,7 @@ public class VehicleDAO {
             ResultSet resultSet = preparedStatement.executeQuery();
 
             while (resultSet.next()) {
-                // driver.setLicenceExpiryDate(LocalDate.parse(resultSet.getString("licence_expire_date")));
-                // driver.setLicenceIssueDate(LocalDate.parse(resultSet.getString("licence_issue_date")));
+
                 vehicle.setUserId(resultSet.getString("user_id"));
             }
         } catch (SQLException e) {
@@ -33,11 +32,11 @@ public class VehicleDAO {
     public void insertVehicleInfo(Vehicle vehicle) {
         Connection dbConn = Database.getConnection();
 
-        String sql = "INSERT INTO vehicle(user_id, vehicle_no) VALUES (?, ?)";
+        String sql = "INSERT INTO vehicle(nic, register_no) VALUES (?, ?)";
 
         try {
             PreparedStatement preparedStatement = dbConn.prepareStatement(sql);
-            preparedStatement.setString(1, vehicle.getUserId());
+            preparedStatement.setString(1, vehicle.getNic());
             preparedStatement.setString(2, vehicle.getVehicleNo());
 
             preparedStatement.executeUpdate();
@@ -57,13 +56,43 @@ public class VehicleDAO {
             ResultSet resultSet = preparedStatement.executeQuery();
 
             while (resultSet.next()) {
-                // driver.setLicenceExpiryDate(LocalDate.parse(resultSet.getString("licence_expire_date")));
-                // driver.setLicenceIssueDate(LocalDate.parse(resultSet.getString("licence_issue_date")));
+ 
                 vehicle.setUserId(resultSet.getString("user_id"));
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
 
+    }
+
+    public void fetchVehicleInfo(Vehicle vehicle) {
+        Connection dbConn = Database.getConnection();
+
+        String sql = "SELECT * FROM vehicle WHERE nic = ?";
+
+        try {
+            PreparedStatement preparedStatement = dbConn.prepareStatement(sql);
+            preparedStatement.setString(1, vehicle.getNic());
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                vehicle.setRegisterNo(resultSet.getString("register_no"));
+                vehicle.setChassisNo(resultSet.getString("chassis_no"));
+                vehicle.setCurrentOwnerName(resultSet.getString("current_owner_name"));
+                vehicle.setCurrentOwnerName(resultSet.getString("current_owner_address"));
+                vehicle.setCurrentOwnerId(resultSet.getString("current_owner_id"));
+                vehicle.setEngineNo(resultSet.getString("engine_no"));
+                vehicle.setVehicleClass(resultSet.getString("vehicle_class"));
+                vehicle.setStateWhenRegister(resultSet.getString("state_when_register"));
+                vehicle.setColor(resultSet.getString("color"));
+                vehicle.setEngineCapacity(resultSet.getInt("engine_capacity"));
+                vehicle.setFuelType(resultSet.getString("fuel_type"));
+                vehicle.setYearOfManufacture(resultSet.getInt("year_of_manufacture"));
+                vehicle.setPreviousOwners(resultSet.getString("previous_owners"));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
