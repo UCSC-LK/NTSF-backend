ab7ceed766a1d8766b1780b8ff4d37a3b9b314f2
AvishiGan <avishiganepola@gmail.com>
2023-03-05 17:50:29 +0530

    Add logic to create offence list along with point weights

--- src/main/java/com/cops/ntsf/model/PointSystem.java
+++ src/main/java/com/cops/ntsf/model/PointSystem.java
@@ -1,27 +1,24 @@
 package com.cops.ntsf.model;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class PointSystem {
-    private ArrayList<Point> points;
+    private final List<Offence> offenceList;
 
     public PointSystem() {
-        this.points = new ArrayList<Point>();
+        offenceList = new ArrayList<>();
+
+        // Initialize offenceList and their corresponding amount and point weight
+        offenceList.add(new Offence("Speeding", 100, 2));
+        offenceList.add(new Offence("Running a red light", 150, 3));
+        offenceList.add(new Offence("Driving under the influence", 500, 5));
+        // Add more offenceList as needed
     }
 
-    public void addPoint(Point point) {
-        this.points.add(point);
+    public List<Offence> getOffences() {
+        return offenceList;
     }
 
-    public void removePoint(Point point) {
-        this.points.remove(point);
-    }
-
-    public double distanceBetween(Point point1, Point point2) {
-        return point1.distanceTo(point2);
-    }
-
-    public ArrayList<Point> getPoints() {
-        return this.points;
-    }
+    // You can add other methods as needed, such as methods to get a violation by name or to calculate the total fine and points for a given list of offenceList.
 }
