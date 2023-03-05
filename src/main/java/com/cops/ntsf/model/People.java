c2104d5d25e0a041f60401f5f53c6be15b71dc28
AvishiGan <avishiganepola@gmail.com>
2023-03-06 03:36:35 +0530

    Import PeopleDAO class in People model

--- src/main/java/com/cops/ntsf/model/People.java
+++ src/main/java/com/cops/ntsf/model/People.java
@@ -1,10 +1,14 @@
 package com.cops.ntsf.model;
 
+import com.cops.ntsf.dao.PeopleDAO;
+
 public class People {
 
     public People(String nic) {
     }
 
     public void getCivilInfo() {
+        PeopleDAO peopleDAO = new PeopleDAO();
+        peopleDAO.fetchCivilInfo(this);
     }
 }
