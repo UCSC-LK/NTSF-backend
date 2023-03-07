ccebee215da47ca575fdb10cfa19b30e897901e9
AvishiGan <avishiganepola@gmail.com>
2023-03-07 09:26:38 +0530

    Remove unnecessary code lines from user servlet

--- src/main/java/com/cops/ntsf/controller/UserServlet.java
+++ src/main/java/com/cops/ntsf/controller/UserServlet.java
@@ -16,12 +16,9 @@ public class UserServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         // Get request parameters
-//        UserType userType = UserType.fromId(Integer.parseInt(req.getParameter("user_type")));
-//        String userId = req.getParameter("user_id");
         String nic = req.getParameter("nic");
 
         UserService userService = new UserService();
-//        User user = userService.getUserInfo(userId);
         User user = userService.getUserInfo(nic);
 
         // Output response
@@ -32,28 +29,4 @@ public class UserServlet extends HttpServlet {
         out.write(new Gson().toJson(user));
         out.close();
     }
-
-
-//    @Override
-//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//
-//        // Get request parameters
-//        String userId = req.getParameter("user_id");
-////        UserType userType = UserType.fromId(Integer.parseInt(req.getParameter("user_type")));
-//        String mobileNo = req.getParameter("mobile_no");
-//        String email = req.getParameter("email");
-//        String name = req.getParameter("name");
-//        String address = req.getParameter("address");
-//
-//        UserService userService = new UserService();
-//        User user = userService.updateUserInfo(userId, mobileNo, email, name, address);
-//
-//        // Output response
-//        PrintWriter out = resp.getWriter();
-//        resp.setContentType("application/json");
-//        resp.setCharacterEncoding("utf-8");
-//
-//        out.write(new Gson().toJson(user));
-//        out.close();
-//    }
 }
