8868a133ff70ac6bf8bc30b498e24a2bf887b4b5
AvishiGan <avishiganepola@gmail.com>
2023-03-07 09:28:40 +0530

    Remove user type related code lines from user service

--- src/main/java/com/cops/ntsf/service/UserService.java
+++ src/main/java/com/cops/ntsf/service/UserService.java
@@ -14,70 +14,21 @@ public class UserService {
         if (nic != null) {
             Auth auth = new Auth(userId, password);
             auth.setAuthInfo();
-//            switch (userType) {
-//                case DRIVER:
-//                    Driver driver = new Driver(nic, loginId);
-//                    driver.setDriverInfo();
-//                    return user;
-//                case PEDESTRIAN:
-//                    Pedestrian pedestrian = new Pedestrian(nic, loginId);
-//                    pedestrian.setPedestrianInfo();
-//                    return pedestrian;
-//                case VEHICLE:
-//                    Vehicle vehicle = new Vehicle(nic, loginId);
-//                    vehicle.setVehicleInfo();
-//                    return user;
-//                default:
-//                    throw new RuntimeException();
-//            }
         }
         return user;
     }
-
-//    public User getUserInfo(String userId) {
-//        User user = new User(userId);
-//        user.getUserInfo();
-//
-//        String nic = user.getNic();
-//
-//        if (nic != null) {
-////            switch (userType) {
-////                case DRIVER:
-//            Driver driver = new Driver(nic);
-//            driver.getDriverInfo();
-//            Vehicle vehicle = new Vehicle(nic);
-//            vehicle.getVehicleInfo();
-//            People people = new People(nic);
-//            people.getCivilInfo();
-////            return user;
-//////                default:
-//////                    throw new RuntimeException();
-//////            }
-////        }
-//        }
-//        return user;
-//    }
 
     public User getUserInfo(String nic) {
         User user = new User(nic);
         user.getUserInfo();
 
-//        String nic = user.getNic();
-
         if (nic != null) {
-//            switch (userType) {
-//                case DRIVER:
             Driver driver = new Driver(nic);
             driver.getDriverInfo();
             Vehicle vehicle = new Vehicle(nic);
             vehicle.getVehicleInfo();
             People people = new People(nic);
             people.getCivilInfo();
-//            return user;
-////                default:
-////                    throw new RuntimeException();
-////            }
-//        }
         }
         return user;
     }
