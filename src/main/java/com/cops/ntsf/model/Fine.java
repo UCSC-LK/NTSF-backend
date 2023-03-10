4466646ab59032d07b292fab6517d3da8640f691
Suraif Muhammad <muhammadsuraif2000@gmail.com>
2023-03-10 18:24:57 +0530

    declare variables in model

--- src/main/java/com/cops/ntsf/model/Fine.java
+++ src/main/java/com/cops/ntsf/model/Fine.java
@@ -1,89 +1,25 @@
 package com.cops.ntsf.model;
 
-import org.json.JSONObject;
+import java.time.LocalDateTime;
 
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.Base64;
+public class Fine {
+    private int fineNo;
 
-public class Fine extends HttpServlet {
-    protected void addFine(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        try{
+    private int offenceNo;
 
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-    }
+    private String userId;
 
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("Came until the doPOST in Fine Servlet");
-        String action = request.getParameter("action");
-        String contentType = request.getHeader("Content-type");
-        String authorizationHeader = request.getHeader("Authorization");
-        System.out.println("Authorization Header: " + authorizationHeader);
-        System.out.println("Content Type: " + contentType);
-        System.out.println("Action: " + action);
+    private String spotDescription;
 
-        String jwt = null;
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            jwt = authorizationHeader.substring(7); //Extracts the jwt token removing the word Bearer prefix
-            System.out.println(jwt);
+    private LocalDateTime dateTime;
 
-            String[] jwtParts = jwt.split("\\.");
-            String headerJson = new String(Base64.getUrlDecoder().decode(jwtParts[0]));
-            String payloadJson = new String(Base64.getUrlDecoder().decode(jwtParts[1]));
-            System.out.println(headerJson);
-            System.out.println(payloadJson);
+    private LocalDateTime dueDateTime;
 
+    private int paymentStatus;
 
-            String signature = jwtParts[2];
-            String unsignedJwt = jwtParts[0] + "." + jwtParts[1];
+    private String policeId;
 
-            String calculatedSignature;
-            try {
-                Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
-                SecretKeySpec secret_key = new SecretKeySpec("mysecret".getBytes(), "HmacSHA256");
-                sha256_HMAC.init(secret_key);
-
-                calculatedSignature = Base64.getUrlEncoder().encodeToString(sha256_HMAC.doFinal(unsignedJwt.getBytes()));
-
-            } catch (Exception e) {
-                throw new RuntimeException("Failed to calculate HMAC: " + e.getMessage());
-            }
-
-            if (!signature.equals(calculatedSignature)) {
-                throw new RuntimeException("JWT signature verification failed as the signature is not matching");
-            } else {
-                System.out.println("JWT signature verification success");
-                JSONObject payloadJsonObject = new JSONObject(payloadJson);
-                String authorizedRank = payloadJsonObject.getString("rank");
-                String authorizedPosition = payloadJsonObject.getString("position");
-                System.out.println(authorizedRank);
-                if (authorizedRank.equals("policeman")) {
-                    if(authorizedPosition.equals("trafficPolice")){
-                        if(action.equals("addFine")){
-                            addFine(request, response);
-                        }
-                    }
-                    else {
-                        System.out.println("You are not authorized to access this page");
-                    }
-                }
-                else {
-                    System.out.println("You are not authorized to access this page");
-                }
-            }
-
-        }
-        else {
-            System.out.println("JWT signature verification failed");
-        }
-    }
+    private String policeStation;
 
 
 }
