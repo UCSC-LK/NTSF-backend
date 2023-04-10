2aedb7346228d282c5ba2c690d77357c1e69be08
AvishiGan <avishiganepola@gmail.com>
2023-04-10 08:35:46 +0530

    Changes made after merging

--- src/main/java/com/cops/ntsf/service/FineService.java
+++ src/main/java/com/cops/ntsf/service/FineService.java
@@ -1,5 +1,50 @@
 package com.cops.ntsf.service;
 
+import com.cops.ntsf.constants.OffenceType;
+import com.cops.ntsf.constants.PaymentStatus;
+
+import java.sql.Date;
+
+public class FineService {
+    public Fine insertFineInfo(String nic,
+                               Integer ticketNo,
+                               Integer fineNo,
+                               Date date,
+                               Date dueDate,
+                               PaymentStatus paymentStatus,
+                               OffenceType offenceType,
+                               String Amount,
+                               Integer pointWeight) {
+        Fine fine = new Fine(nic, ticketNo, fineNo, date, dueDate, paymentStatus, offenceType, Amount, pointWeight);
+        fine.setFineInfo();
+
+        return fine;
+    }
+}
+//package com.cops.ntsf.service;
+//
+//import com.cops.ntsf.constants.OffenceType;
+//import com.cops.ntsf.constants.PaymentStatus;
+//import com.cops.ntsf.model.Fine;
+//
+//import java.sql.Date;
+//
+//public class FineService {
+//    public Fine insertFineInfo(String nic,
+//                               Integer ticketNo,
+//                               Integer fineNo,
+//                               Date date,
+//                               Date dueDate,
+//                               PaymentStatus paymentStatus,
+//                               OffenceType offenceType,
+//                               String Amount,
+//                               Integer pointWeight) {
+//        Fine fine = new Fine(nic, ticketNo, fineNo, date, dueDate, paymentStatus, offenceType, Amount, pointWeight);
+//        fine.setFineInfo();
+//
+//        return fine;
+//    }
+//}
 //public class FineService {
 //
 //    public Fine insertFineInfo(Integer fineNo, String fineType, String userId, Integer fineAmount, String paymentStatus, String policeId) throws ParseException {
