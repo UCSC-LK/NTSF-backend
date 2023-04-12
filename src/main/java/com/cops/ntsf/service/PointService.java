48ef6ef40087197d0e54b13b90605af8558a1642
AvishiGan <avishiganepola@gmail.com>
2023-04-12 07:19:12 +0530

    Create reducePoints function in PointService

--- src/main/java/com/cops/ntsf/service/PointService.java
+++ src/main/java/com/cops/ntsf/service/PointService.java
@@ -1,11 +1,42 @@
 package com.cops.ntsf.service;
 
+import com.cops.ntsf.model.Fine;
+import com.cops.ntsf.model.Offence;
 import com.cops.ntsf.model.Point;
 
 import java.sql.SQLException;
 
 public class PointService {
     public Point getPointInfo(String nic) {
+        Point point = new Point(nic);
+        try {
+            point.getPointInfo();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return point;
+    }
+
+    public Point reducePoints(String nic, String fineNo) {
+        Fine fine = new Fine(Integer.parseInt(fineNo), nic);
+        if (fine.getFineByFineNo() != null) {
+            Offence offence = new Offence(fine.getOffenceNo());
+            offence.getOffence();
+            int pointsToBeDone = offence.getDemerit_points();
+
+            Point point = new Point();
+            int currentPoints = point.getCurrentPoints();
+
+            // Reduce points
+            currentPoints = currentPoints - pointsToBeDone;
+
+            point.updateCurrentPoints(currentPoints);
+
+            return point;
+        }
+
+
         Point point = new Point(nic);
         try {
             point.getPointInfo();
