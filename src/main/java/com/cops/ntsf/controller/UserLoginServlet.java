109af6fa4ee797bce8e141eb5a1e4d92e78dda7f
AvishiGan <avishiganepola@gmail.com>
2023-04-15 09:09:05 +0530

    Create new Validator object in Login and SignUp servlets and recall validation functions

--- src/main/java/com/cops/ntsf/controller/UserLoginServlet.java
+++ src/main/java/com/cops/ntsf/controller/UserLoginServlet.java
@@ -1,6 +1,7 @@
 package com.cops.ntsf.controller;
 
 import com.cops.ntsf.service.AuthService;
+import com.cops.ntsf.util.Validator;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -52,58 +53,13 @@ public class UserLoginServlet extends HttpServlet {
     }
 
     public boolean checkValidations(String nic, String password) {
-        if (checkNICValidation(nic)) {
-            checkPasswordValidation(password);
-        }
-        return false;
-    }
 
-    /*
-    @ Validate NIC
-    * */
-    public boolean checkNICValidation(String nic) {
+        Validator validator = new Validator();
 
-        if (nic.trim().equals("")) {
-            System.out.println("NIC is empty");
-        } else if (nic.length() == 10 && nic.substring(0, 9).matches("[0-9]+") && !Character.isLetter(nic.charAt(9)) && (nic.charAt(9) == 'x' || nic.charAt(9) == 'v')) {
-            System.out.println("NIC is valid");
-            return true;
-        } else if (nic.length() == 12 && nic.matches("[0-9]+")) {
-            System.out.println("NIC is valid");
+        if (validator.checkNICValidation(nic)) {
+            validator.checkPasswordValidation(password);
             return true;
         }
         return false;
-    }
-
-    /*
-    @ Validate password
-    * */
-    public void checkPasswordValidation(String password) {
-
-        if (password == null || password.length() < 8) {
-            System.out.println("Password is not valid");
-            return;
-        }
-
-        boolean hasUppercase = false;
-        boolean hasLowercase = false;
-        boolean hasDigit = false;
-        boolean hasSpecialChar = false;
-
-        for (char c : password.toCharArray()) {
-            if (Character.isUpperCase(c)) {
-                hasUppercase = true;
-            } else if (Character.isLowerCase(c)) {
-                hasLowercase = true;
-            } else if (Character.isDigit(c)) {
-                hasDigit = true;
-            } else {
-                hasSpecialChar = true;
-            }
-        }
-
-        if (!hasUppercase || !hasLowercase || !hasDigit || !hasSpecialChar) {
-            System.out.println("Password should contain at least one uppercase letter, one lowercase letter, one digit, and one special character");
-        }
     }
 }
--- src/main/java/com/cops/ntsf/controller/UserSignUpServlet.java
+++ src/main/java/com/cops/ntsf/controller/UserSignUpServlet.java
@@ -2,6 +2,7 @@ package com.cops.ntsf.controller;
 
 import com.cops.ntsf.model.User;
 import com.cops.ntsf.service.UserService;
+import com.cops.ntsf.util.Validator;
 import com.google.gson.Gson;
 
 import javax.servlet.http.HttpServlet;
@@ -9,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Arrays;
 
 public class UserSignUpServlet extends HttpServlet {
     @Override
@@ -36,110 +36,19 @@ public class UserSignUpServlet extends HttpServlet {
     }
 
     private boolean checkValidations(String nic, String email, String mobileNo, String password) {
-        return checkNICValidation(nic) && checkEmailValidation(email) && checkMobileNoValidation(mobileNo) && checkPasswordValidation(password);
-    }
 
-    /*
-    @ Validate NIC
-    * */
-    public boolean checkNICValidation(String nic) {
+        Validator validator = new Validator();
 
-        if (nic.trim().equals("")) {
-            System.out.println("NIC is empty");
-        } else if (nic.length() == 10 && nic.substring(0, 9).matches("[0-9]+") && !Character.isLetter(nic.charAt(9)) && (nic.charAt(9) == 'x' || nic.charAt(9) == 'v')) {
-            System.out.println("NIC is valid");
-            return true;
-        } else if (nic.length() == 12 && nic.matches("[0-9]+")) {
-            System.out.println("NIC is valid");
-            return true;
-        }
-        return false;
-    }
-
-    /*
-   @ Validate password
-   * */
-    public boolean checkPasswordValidation(String password) {
-
-        if (password == null || password.length() < 8) {
-            System.out.println("Password is not valid");
-            return false;
-        }
-
-        boolean hasUppercase = false;
-        boolean hasLowercase = false;
-        boolean hasDigit = false;
-        boolean hasSpecialChar = false;
-
-        for (char c : password.toCharArray()) {
-            if (Character.isUpperCase(c)) {
-                hasUppercase = true;
-            } else if (Character.isLowerCase(c)) {
-                hasLowercase = true;
-            } else if (Character.isDigit(c)) {
-                hasDigit = true;
-            } else {
-                hasSpecialChar = true;
+        if (validator.checkNICValidation(nic)) {
+            validator.checkEmailValidation(email);
+            {
+                validator.checkMobileNoValidation(mobileNo);
+                {
+                    validator.checkPasswordValidation(password);
+                }
             }
-        }
-
-        if (!hasUppercase || !hasLowercase || !hasDigit || !hasSpecialChar) {
-            System.out.println("Password should contain at least one uppercase letter, one lowercase letter, one digit, and one special character");
-            return false;
-        }
-        return false;
-    }
-
-    /*
-    @ Validate Mobile No in Sri Lankan format
-    * */
-    public boolean checkMobileNoValidation(String mobileNo) {
-        if (mobileNo == null || mobileNo.isEmpty()) {
-            System.out.println("Mobile No is empty");
-            return false;
-        }
-
-        // Remove any leading plus sign
-        if (mobileNo.startsWith("+")) {
-            mobileNo = mobileNo.substring(1);
-        }
-
-        // Check length and format
-        if (mobileNo.length() != 9 || !mobileNo.matches("\\d{9}")) {
-            return false;
-        }
-
-        // Check for valid network operator codes
-
-        // Declares a String array named validOperatorCodes
-        // Initializes it with five valid network operator codes in Sri Lanka
-        String[] validOperatorCodes = {"071", "072", "075", "077", "078"};
-
-        // Extracts the first 3 digits of the phone number and assigns them to a new String variable named operatorCode
-        String operatorCode = mobileNo.substring(0, 3);
-
-        // Checks whether the operatorCode is in the list of validOperatorCodes
-        if (!Arrays.asList(validOperatorCodes).contains(operatorCode)) {
-            return false;
-        }
-
-        // All validation checks have passed
-        return true;
-    }
-
-    /*
-    @ Validate email
-    * */
-    public boolean checkEmailValidation(String email) {
-        if (email.trim().equals("")) {
-            System.out.println("Email is empty");
-            return false;
-        } else if (!email.trim().matches("^[A-Za-z0-9+_.-]+@(.+)$")) {
-            System.out.println("Email is invalid");
-            return false;
-        } else {
-            System.out.println("Email is valid");
             return true;
         }
+        return false;
     }
 }
