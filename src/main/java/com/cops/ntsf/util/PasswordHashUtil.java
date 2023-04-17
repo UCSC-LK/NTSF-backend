95881caf6d49dd4686b27a307c61e25172e39e7f
AvishiGan <avishiganepola@gmail.com>
2023-04-17 09:51:54 +0530

    Replace "UTF-8" from "StandardCharsets.UTF_8" in hashingPassword function

--- src/main/java/com/cops/ntsf/util/PasswordHashUtil.java
+++ src/main/java/com/cops/ntsf/util/PasswordHashUtil.java
@@ -1,14 +1,14 @@
 package com.cops.ntsf.util;
 
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.util.Base64;
 
 public class PasswordHashUtil {
     public static String hashingPassword(String password) throws Exception {
-        String originalString = password;
-        System.out.println("Original String to hash: " + originalString);
+        System.out.println("Original String to hash: " + password);
         MessageDigest digest = MessageDigest.getInstance("SHA-256");
-        byte[] hash = digest.digest(originalString.getBytes("UTF-8"));
+        byte[] hash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
         String encoded = Base64.getEncoder().encodeToString(hash);
         System.out.println("Hash: " + encoded);
         return encoded;
