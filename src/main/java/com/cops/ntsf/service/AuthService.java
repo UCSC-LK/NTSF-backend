65caf19decba6ebaf9e862e60389b65a377f044f
AvishiGan <avishiganepola@gmail.com>
2023-04-17 06:59:28 +0530

    Add jwt to AuthService

--- src/main/java/com/cops/ntsf/service/AuthService.java
+++ src/main/java/com/cops/ntsf/service/AuthService.java
@@ -1,7 +1,9 @@
 package com.cops.ntsf.service;
 
+import com.cops.ntsf.constants.AuthType;
 import com.cops.ntsf.model.Auth;
 import com.cops.ntsf.model.User;
+import com.cops.ntsf.util.JwtUtils;
 import org.json.JSONObject;
 
 public class AuthService {
@@ -19,15 +21,23 @@ public class AuthService {
         auth.setUserId(userId);
         auth.getAuthFromUserId();
 
+        boolean loggedIn = verifyPassword(auth.getPassword(), inputPassword);
+
         JSONObject loginResponse = new JSONObject();
         loginResponse.put("loggedIn", verifyPassword(auth.getPassword(), inputPassword));
         loginResponse.put("userId", userId);
+
+        if (loggedIn) {
+            JwtUtils jwtUtils = new JwtUtils(AuthType.USER, null);
+            loginResponse.put("jwt", jwtUtils.generateJwt());
+        }
 
         return loginResponse.toString();
     }
 
     public Boolean verifyPassword(String password, String inputPassword) {
-        // System.out.println(password + inputPassword);
+        if (password == null || inputPassword == null)
+            return false;
         return password.equals(inputPassword);
     }
 }
