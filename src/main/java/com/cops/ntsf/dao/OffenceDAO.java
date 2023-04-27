e1892d691f13eadb7450b1120b03296dc9332a4d
Suraif Muhammad <muhammadsuraif2000@gmail.com>
2023-04-27 22:36:35 +0530

    fetchoffence dao completed

--- src/main/java/com/cops/ntsf/dao/OffenceDAO.java
+++ src/main/java/com/cops/ntsf/dao/OffenceDAO.java
@@ -178,4 +178,47 @@ public class OffenceDAO {
             throw new RuntimeException(e);
         }
     }
+
+    public JSONArray fetchOffenceDetailsList(int offence_no) {
+        Connection dbConn = null;
+
+        JSONArray jsonArray = new JSONArray();
+
+        try {
+            dbConn = Database.getConnection();
+            String sql = "SELECT * FROM offence WHERE offence_no = ?";
+            PreparedStatement preparedStatement = dbConn.prepareStatement(sql);
+            preparedStatement.setInt(1, offence_no);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+//                int offence_no = resultSet.getInt("offence_no");
+                String offence_type = resultSet.getString("offence_type");
+                String description = resultSet.getString("description");
+                int amount = resultSet.getInt("amount");
+                int demerit_points = resultSet.getInt("demerit_points");
+
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("offence_no", offence_no);
+                jsonObject.put("offence_type", offence_type);
+                jsonObject.put("description", description);
+                jsonObject.put("amount", amount);
+                jsonObject.put("demerit_points", demerit_points);
+
+                jsonArray.put(jsonObject);
+
+                System.out.println("Offence No: " + offence_no);
+                System.out.println("Offence Type: " + offence_type);
+                System.out.println("Description: " + description);
+                System.out.println("Amount: " + amount);
+                System.out.println("Demerit Points: " + demerit_points);
+            }
+            resultSet.close();
+            preparedStatement.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return jsonArray;
+    }
+
 }
