c9f39981dd475d896c93ec3c39733c5d2bf99051
AvishiGan <avishiganepola@gmail.com>
2023-04-28 00:07:07 +0530

    Change of logic in FineService to get finesList including offence object inside it

--- src/main/java/com/cops/ntsf/service/FineService.java
+++ src/main/java/com/cops/ntsf/service/FineService.java
@@ -7,47 +7,18 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class FineService {
+    public ArrayList<Fine> getFinesInfo(String nic) throws SQLException {
+        Fine fineObj = new Fine(nic);
+        ArrayList<Fine> finesList = fineObj.getUserFinesInfo();
 
-    /**
-     * Made the function static and variables final
-     */
-    public static class FinesInfo {
-        private final ArrayList<Fine> fines;
-        private final String offenceType;
-        private final String amount;
+        for (Fine fine : finesList) {
+            String offenceNo = fine.getOffenceNo();
+            Offence offence = new Offence(offenceNo);
+            offence.getOffence();
 
-        public FinesInfo(ArrayList<Fine> fines, String offenceType, String amount) {
-            this.fines = fines;
-            this.offenceType = offenceType;
-            this.amount = amount;
+            fine.setOffence(offence);
         }
 
-        public ArrayList<Fine> getFines() {
-            return fines;
-        }
-
-        public String getOffenceType() {
-            return offenceType;
-        }
-
-        public String getAmount() {
-            return amount;
-        }
-    }
-
-    public FinesInfo getFinesInfo(String nic) throws SQLException {
-        Fine fine = new Fine(nic);
-        fine.getUserFinesInfo();
-
-        String offenceNo = fine.getOffenceNo();
-
-        Offence offence = new Offence(offenceNo);
-        String offenceType = offence.getOffence_type();
-        String amount = String.valueOf(offence.getAmount());
-
-        ArrayList<Fine> fines = new ArrayList<>();
-        fines.add(fine);
-
-        return new FinesInfo(fines, offenceType, amount);
+        return finesList;
     }
 }
