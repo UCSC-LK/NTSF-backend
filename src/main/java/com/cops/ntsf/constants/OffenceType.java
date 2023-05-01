a5ff274d64edd63ded61d7f53cf9643e829de42f
AvishiGan <avishiganepola@gmail.com>
2023-05-01 20:18:47 +0530

    Add code to convert type of offence from string to OffenceType

--- src/main/java/com/cops/ntsf/constants/OffenceType.java
+++ src/main/java/com/cops/ntsf/constants/OffenceType.java
@@ -8,4 +8,22 @@ public enum OffenceType {
     public static OffenceType fromId(int offenceType) {
         return OffenceType.values()[offenceType - 1];
     }
+
+    public static OffenceType fromString(String offenceTypeString) {
+        OffenceType offenceType = null;
+        switch (offenceTypeString) {
+            case "driver":
+                offenceType = DRIVER;
+                break;
+            case "vehicle":
+                offenceType = VEHICLE;
+                break;
+            case "pedestrian":
+                offenceType = PEDESTRIAN;
+                break;
+            default:
+                break;
+        }
+        return offenceType;
+    }
 }
