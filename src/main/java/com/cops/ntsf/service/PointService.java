820016783b39ec524cd5d29106fa97525a9b7b24
AvishiGan <avishiganepola@gmail.com>
2023-05-09 16:02:29 +0530

    Add reducePoints function

--- src/main/java/com/cops/ntsf/service/PointService.java
+++ src/main/java/com/cops/ntsf/service/PointService.java
@@ -1,55 +1,65 @@
 package com.cops.ntsf.service;
 
-import com.cops.ntsf.model.Fine;
-import com.cops.ntsf.model.Offence;
-import com.cops.ntsf.model.Point;
-
-import java.sql.SQLException;
+import com.cops.ntsf.model.PointData;
 
 public class PointService {
-    public Point getPointInfo(String nic) {
-        Point point = new Point(nic);
-        try {
-            point.getPointInfo();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+    public void reducePoints(Integer demeritPoints, String licenseNo) {
+        PointData pointData = new PointData();
+        int currentPoints = pointData.getCurrentPointsByNic(licenseNo);
 
-        return point;
+        if (checkCurrentPoints(currentPoints)) {
+            // Reduce points
+            currentPoints = currentPoints - demeritPoints;
+            pointData.updateCurrentPoints(currentPoints);
+        } else {
+            System.out.println("Licence got declined");
+        }
     }
 
-    public Point reducePoints(String nic, String fineNo) {
-        Fine fine = new Fine(Integer.parseInt(fineNo), nic);
-        if (fine.getFineByFineNo() != null) {
-            Offence offence = new Offence(fine.getOffenceNo());
-            offence.getOffence();
-            int pointsToBeDone = offence.getDemerit_points();
-
-            Point point = new Point();
-            int currentPoints = point.getCurrentPoints();
-
-            if (checkCurrentPoints(currentPoints)) {
-                // Reduce points
-                currentPoints = currentPoints - pointsToBeDone;
-                point.updateCurrentPoints(currentPoints);
-            } else
-                System.out.println("Licence got declined");
-
-            return point;
-        }
-
-        Point point = new Point(nic);
-        try {
-            point.getPointInfo();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-        return point;
-    }
-
-    // Check whether current points is less than or equal to 0
+    // Check whether current point is less than or equal to 0
     public boolean checkCurrentPoints(int currentPoints) {
         return currentPoints > 0;
     }
+
+    //    public PointData getPointInfo(String nic) {
+//        PointData pointData = new PointData(nic);
+//        try {
+//            pointData.getPointInfo();
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        return pointData;
+//    }
+
+//    public PointData reducePoints(String nic, String fineNo) {
+//        Fine fine = new Fine(Integer.parseInt(fineNo), nic);
+//        if (fine.getFineByFineNo() != null) {
+//            Offence offence = new Offence(fine.getOffenceNo());
+//            offence.getOffence();
+//            int pointsToBeDone = offence.getDemerit_points();
+//
+//            PointData pointData = new PointData();
+//            int currentPoints = pointData.getCurrentPoints();
+//
+//            if (checkCurrentPoints(currentPoints)) {
+//                // Reduce points
+//                currentPoints = currentPoints - pointsToBeDone;
+//                pointData.updateCurrentPoints(currentPoints);
+//            } else
+//                System.out.println("Licence got declined");
+//
+//            return pointData;
+//        }
+//
+//        PointData pointData = new PointData(nic);
+//        try {
+//            pointData.getPointInfo();
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        return pointData;
+//    }
 }
+
